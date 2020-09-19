af908b8aaca714675f31f74272af70e7b377f414
ZEF-hun <gerasenkov1@mail.ru>
2020-09-19 12:41:31 +0300

    Create Bot.py

---
+++ Bot.py
@@ -0,0 +1,20 @@
+import telebot
+
+bot = telebot.TeleBot('1246639039:AAGXABe2xAj33-N7Auqld5J9ZRTXa6NdtDY')
+
+keyboard = telebot.types.ReplyKeyboardMarkup(True, True)
+keyboard.row('Привет', 'Пока')
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет, ты написал мне /start', reply_markup=keyboard)
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.chat.id, 'Привет, пользователь')
+    elif message.text.lower() == 'пока':
+        bot.send_message(message.chat.id, 'Прощай, пользователь')
+
+
+bot.polling()
