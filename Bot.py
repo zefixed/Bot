f371dfdce8e858899e2ac96ad023821ea3b9a9f0
ZEF-hun <gerasenkov1@mail.ru>
2020-10-10 14:02:40 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -5,6 +5,8 @@ bot = telebot.TeleBot('1246639039:AAGXABe2xAj33-N7Auqld5J9ZRTXa6NdtDY')
 keyboard = telebot.types.ReplyKeyboardMarkup(True, True)
 keyboard.row('Привет', 'Пока')
 
+name = ''
+
 @bot.message_handler(commands=['start', 'help', 'reg'])
 def start_help_message(message):
     if message.text == '/start':
@@ -12,14 +14,23 @@ def start_help_message(message):
     elif message.text == '/help':
         bot.send_message(message.chat.id, 'В разработке')
     elif message.text == '/reg':
-        bot.send_message(message.chat.id, 'В разработке2')
+        name = bot.send_message(message.chat.id, 'Как мне к вам обращаться?')
+        bot.register_next_step_handler(name, hello_message)
+def hello_message(message):
+    global name
+    name = message.text
+    bot.send_message(message.chat.id, 'Очень приятно, {name}. Рад тебя видеть.'.format(name=message.text), reply_markup=keyboard)
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
-    if message.text.lower() == 'привет':
+    if message.text.lower() == 'привет' and name == '':
         bot.send_message(message.chat.id, 'Привет, если хочешь чтобы я к тебе обращался по имени введи /reg')
-    elif message.text.lower() == 'пока':
-        bot.send_message(message.chat.id, 'Прощай, Пользователь')
+    elif message.text.lower() == 'привет' and name != '':
+        bot.send_message(message.chat.id, 'Привет, {}'.format(name))
+    elif message.text.lower() == 'пока' and name == '':
+        bot.send_message(message.chat.id, 'Прощай, Аноним')
+    elif message.text.lower() == 'пока' and name != '':
+        bot.send_message(message.chat.id, 'Прощай, {}'.format(name))
 
 
 bot.polling()
