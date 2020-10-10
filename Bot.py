12e937ad74776c5a6c7ee7ffcaf38c95fb3a2411
ZEF-hun <gerasenkov1@mail.ru>
2020-10-10 14:50:31 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,9 +1,16 @@
 import telebot
+from telebot import types
 
 bot = telebot.TeleBot('1246639039:AAGXABe2xAj33-N7Auqld5J9ZRTXa6NdtDY')
 
 keyboard = telebot.types.ReplyKeyboardMarkup(True, True)
 keyboard.row('Привет', 'Пока')
+#keyboard1 = types.InlineKeyboardMarkup()
+#cbbn1 = types.InlineKeyboardButton(text="Хочу", callback_data="хочу")
+#cbbn2 = types.InlineKeyboardButton(text="Не хочу", callback_data="нехочу")
+#keyboard1.add(cbbn1, cbbn2)
+keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True)
+keyboard1.row('Хочу', 'Не хочу')
 
 name = ''
 
@@ -16,6 +23,12 @@ def start_help_message(message):
     elif message.text == '/reg':
         name = bot.send_message(message.chat.id, 'Как мне к вам обращаться?')
         bot.register_next_step_handler(name, hello_message)
+
+@bot.message_handler(commands=['reg'])
+def reg_message(message):
+    name = bot.send_message(message.chat.id, 'Как мне к вам обращаться?')
+    bot.register_next_step_handler(name, hello_message)
+
 def hello_message(message):
     global name
     name = message.text
@@ -24,13 +37,21 @@ def hello_message(message):
 @bot.message_handler(content_types=['text'])
 def send_text(message):
     if message.text.lower() == 'привет' and name == '':
-        bot.send_message(message.chat.id, 'Привет, если хочешь чтобы я к тебе обращался по имени введи /reg')
+        reg = bot.send_message(message.chat.id, 'Привет, если хочешь чтобы я к тебе обращался по имени введи /reg', reply_markup=keyboard1)
+        if message.text.lower == 'хочу':
+            bot.register_next_step_handler(reg, reg_message)
+        elif message.text.lower == 'не хочу':
+            bot.send_message(message.chat.id, 'Ну как хочешь, Аноним')
     elif message.text.lower() == 'привет' and name != '':
         bot.send_message(message.chat.id, 'Привет, {}'.format(name))
     elif message.text.lower() == 'пока' and name == '':
         bot.send_message(message.chat.id, 'Прощай, Аноним')
     elif message.text.lower() == 'пока' and name != '':
         bot.send_message(message.chat.id, 'Прощай, {}'.format(name))
+
+#@bot.callback_query_handler(func=lambda call: True)
+#def hello_text(call):
+#    if call.data == 'хочу':
 
 
 bot.polling()
