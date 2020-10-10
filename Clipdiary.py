1cf260793139b5b2e65c566b0c7ff8668fbf1c89
ZEF-hun <gerasenkov1@mail.ru>
2020-10-10 14:02:43 +0300

    Update Clipdiary.py

--- Clipdiary.py
+++ Clipdiary.py
@@ -2,3 +2,5 @@
 @bot.message_handler(commands=['help'])
 def help_message(message):
     bot.send_message(message.chat.id, 'Test')
+#------------------------------------------------------------------
+bot.send_message(message.chat.id, 'Приятно познакомиться ', name)
