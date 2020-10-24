d71402060fe24d82f4500e0d18b8b1663c3d0a74
ZEF-hun <gerasenkov1@mail.ru>
2020-10-24 15:36:57 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -16,7 +16,6 @@ bot = telebot.TeleBot('1246639039:AAGXABe2xAj33-N7Auqld5J9ZRTXa6NdtDY')
 kb = telebot.types.ReplyKeyboardMarkup(True, True)
 kb.row('Привет', 'Пока')
 
-i = 0
 user_data = {}
 name = ''
 
@@ -25,7 +24,7 @@ class User:
         self.first_name = first_name
         self.last_name = ''
 
-@bot.message_handler(commands=['start', 'help', 'reg','adm'])
+@bot.message_handler(commands=['start', 'help', 'reg','adm', 'rereg'])
 def start_help_message(message):
     try:
         if message.text == '/start':
@@ -34,27 +33,31 @@ def start_help_message(message):
             bot.send_message(message.chat.id, 'Доступные команду\n'
                                               '1. /start (Используется для начала общения со мной)\n'
                                               '2. /help (Показывает известные мне команды)\n'
-                                              '3. /reg (Позволяет мне обращаться к тебе по имени)\n')
+                                              '3. /reg (Позволяет мне обращаться к тебе по имени)\n'
+                                              '4. /rereg (Позволяет перерегистрироваться)\n')
         elif message.text == '/reg':
             name = bot.send_message(message.chat.id, 'Введите имя')
-            bot.register_next_step_handler(name, reg_message)
+            bot.register_next_step_handler(name, reg_firstname_step)
         elif message.text == '/adm':
             adm = bot.send_message(message.chat.id,'Введите пароль')
             bot.register_next_step_handler(adm, admin_panel)
+        elif message.text == '/rereg':
+            name = bot.send_message(message.chat.id, 'Введите имя')
+            bot.register_next_step_handler(name, rereg_firstname_step)
     except Exception as e:
         bot.send_message(message.chat.id, 'Ошибка, {}'.format(e))
 
-def reg_message(message):
+def reg_firstname_step(message):
     try:
         user_id = message.from_user.id
         user_data[user_id] = User(message.text)
 
         msg = bot.send_message(message.chat.id, "Введите фамилию")
-        bot.register_next_step_handler(msg, process_lastname_step)
+        bot.register_next_step_handler(msg, reg_lastname_step)
     except Exception as e:
         bot.send_message(message.chat.id, 'Ошибка, {}'.format(e))
 
-def process_lastname_step(message):
+def reg_lastname_step(message):
     try:
         user_id = message.from_user.id
         user = user_data[user_id]
@@ -70,22 +73,46 @@ def process_lastname_step(message):
     except Exception as e:
         bot.send_message(message.chat.id, 'Ошибка, {}'.format(e))
 
+def rereg_firstname_step(message):
+    try:
+        user_id = message.from_user.id
+        user_data[user_id] = User(message.text)
+
+        msg = bot.send_message(message.chat.id, "Введите фамилию")
+        bot.register_next_step_handler(msg, rereg_lastname_step)
+    except Exception as e:
+        bot.send_message(message.chat.id, 'Ошибка, {}'.format(e))
+
+def rereg_lastname_step(message):
+    user_id = message.from_user.id
+    user = user_data[user_id]
+    user.last_name = message.text
+
+    sql = 'UPDATE users SET first_name = %s, last_name = %s WHERE user_id = {0}'.format(user_id)
+    val = (user.first_name, user.last_name)
+    cursor.execute(sql, val)
+    db.commit()
+
+    bot.send_message(message.chat.id, "Вы успешно перерегистрированны!")
+
 @bot.message_handler(content_types=['text'])
 def send_text(message):
     try:
-        global name, i
-        if message.text.lower() == 'привет' and name == '' and i == 0:
-            bot.send_message(message.chat.id, 'Привет, если хочешь чтобы я к тебе обращался по имени введи /reg (не обязательно)')
-            i = 1
-        elif message.text.lower() == 'привет' and name == '' and i == 1:
-            bot.send_message(message.chat.id, 'Привет, Аноним')
-            name = 'Аноним'
-        elif message.text.lower() == 'привет' and name != '':
+        user_id = message.from_user.id
+
+        sql = 'SELECT first_name FROM users WHERE user_id = {0}'.format(user_id)
+        cursor.execute(sql)
+        sup = cursor.fetchone()
+        if message.text.lower() == 'привет'and sup == None:
+            bot.send_message(message.chat.id,'Привет, если хочешь чтобы я к тебе обращался по имени введи /reg (не обязательно)')
+        elif message.text.lower() == 'привет'and sup != None:
+            name = sup[0]
             bot.send_message(message.chat.id, 'Привет, {}'.format(name))
-        elif message.text.lower() == 'пока' and name == '':
-            bot.send_message(message.chat.id, 'Прощай, Аноним')
-        elif message.text.lower() == 'пока' and name != '':
-            bot.send_message(message.chat.id, 'Прощай, {}'.format(name))
+        if message.text.lower() == 'пока' and sup == None:
+            bot.send_message(message.chat.id,'Пока, Аноним')
+        elif message.text.lower() == 'пока'and sup != None:
+            name = sup[0]
+            bot.send_message(message.chat.id, 'Пока, {}'.format(name))
     except Exception as e :
         bot.send_message(message.chat.id, 'Ошибка, {}'.format(e))
 
