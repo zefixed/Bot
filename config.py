ad5aa52a64d2cac1a6fc32fc2384db44a1d85a37
ZEF-hun <gerasenkov1@mail.ru>
2020-10-31 15:08:45 +0300

    Update config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+import telebot
+
+token = '1246639039:AAGXABe2xAj33-N7Auqld5J9ZRTXa6NdtDY'
+
+kb = telebot.types.ReplyKeyboardMarkup(True, True)
+kb.row('Привет', 'Пока')
+
+kb_admc = telebot.types.ReplyKeyboardMarkup(True, True)
+kb_admc.row('Создать новую запись в БД', 'Удалить запись из БД', 'Выйти')
+
+kb_adm = telebot.types.ReplyKeyboardMarkup(True, True)
+kb_adm.row('/adm')
+
+adm = 'f297a57a5a743894a0e4a801fc3'
