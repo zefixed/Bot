4a4be8617a9496e0461c899fa3ced5d5812e249b
ZEF-hun <gerasenkov1@mail.ru>
2020-10-31 12:11:22 +0300

    Update Clipdiary.py

--- Clipdiary.py
+++ Clipdiary.py
@@ -58,5 +58,6 @@ def send_text(message):
         print(name)
         bot.send_message(message.chat.id, 'Привет, {}'.format(name))
 #-----------------------------------------------------------------------------------------------------------------------
+cursor.execute('CREATE TABLE `articles` (`id` tinyint unsigned NOT NULL auto_increment, `question` varchar(255) default NULL, `answer` text, PRIMARY KEY (`id`), FULLTEXT KEY `ft1` (`question`)) ENGINE=MyISAM DEFAULT CHARSET=utf8;')
 #-----------------------------------------------------------------------------------------------------------------------
 #-----------------------------------------------------------------------------------------------------------------------
