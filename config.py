249772f9b5d45a380a9cf11530dcc36c239c7ea0
ZEF-hun <gerasenkov1@mail.ru>
2020-11-14 14:10:10 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,16 @@
 import telebot
 
-token = '1246639039:AAGXABe2xAj33-N7Auqld5J9ZRTXa6NdtDY'
+token = '1348485436:AAETyTwfpvevMsozjXoQl9WRZptjhAwH-Ks'
+token_old = '1246639039:AAGXABe2xAj33-N7Auqld5J9ZRTXa6NdtDY'
 
 kb = telebot.types.ReplyKeyboardMarkup(True, True)
 kb.row('Привет', 'Пока')
 
 kb_admc = telebot.types.ReplyKeyboardMarkup(True, True)
-kb_admc.row('Создать новую запись в БД','Удалить запись из БД').add('Просмотреть все записи', 'Выйти')
+kb_admc.row('Создать новую запись в БД', 'Удалить запись из БД').add('Просмотреть все записи', 'Выйти')
 
 kb_admbd = telebot.types.ReplyKeyboardMarkup(True, True)
-kb_admbd.row('radical_power_logarithm','trigonometry').add('smth', 'smth').add('smth')
+kb_admbd.row('radical_power_logarithm', 'trigonometry').add('probability_theory', 'geometric_concepts').add('algebraic_concepts')
 
 kb_adm = telebot.types.ReplyKeyboardMarkup(True, True)
 kb_adm.row('/adm')
@@ -24,6 +25,6 @@ kb_ask = telebot.types.ReplyKeyboardMarkup(True, True)
 kb_ask.row('Просмотреть список доступных вопросов', 'Написать вручную')
 
 kb_ask_t = telebot.types.ReplyKeyboardMarkup(True, True)
-kb_ask_t.row('Корни, степени, логарифмы', 'Тригонометрия')
+kb_ask_t.row('Корни, степени, логарифмы', 'Тригонометрия').add('Теория вероятностей', 'Геометрические понятия').add('Алгебраические понятия и интересные Вопросы')
 
 adm = 'f297a57a5a743894a0e4a801fc3'
