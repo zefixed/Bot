cfd14e67786f363e162d0661f5bf236451b33b5e
ZEF-hun <gerasenkov1@mail.ru>
2020-11-15 12:23:42 +0300

    Update Clipdiary.py

--- Clipdiary.py
+++ Clipdiary.py
@@ -8,8 +8,13 @@ msg = bot.send_message(message.chat.id, '')
 #-----------------------------------------------------------------------------------------------------------------------
 bot.register_next_step_handler(msg, )
 #-----------------------------------------------------------------------------------------------------------------------
-cursor.execute('CREATE TABLE `articles` (`id` tinyint unsigned NOT NULL auto_increment, `question` varchar(255) default NULL, `answer` text, PRIMARY KEY (`id`), FULLTEXT KEY `ft1` (`question`)) ENGINE=MyISAM DEFAULT CHARSET=utf8;')
+cursor.execute('CREATE TABLE `articles` (`id` tinyint unsigned NOT NULL auto_increment, `question` varchar(255) default NULL,'
+               ' `answer` text, PRIMARY KEY (`id`), FULLTEXT KEY `ft1` (`question`)) ENGINE=MyISAM DEFAULT CHARSET=utf8;')
 #-----------------------------------------------------------------------------------------------------------------------
+if message.text == '':
+
+elif message.text == '':
+
 #-----------------------------------------------------------------------------------------------------------------------
 #-----------------------------------------------------------------------------------------------------------------------
 #-----------------------------------------------------------------------------------------------------------------------
