43694054ce52a18db868fb85b32cadb1f02e892c
ZEF <gerasencov.code@mail.ru>
2021-02-27 14:19:37 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot import types
 
 token = '1348485436:AAHMeMOVAXSfgVaA0208oy5cdRUGWIN0pIg'
 token_old = '1246639039:AAGXABe2xAj33-N7Auqld5J9ZRTXa6NdtDY'
@@ -38,6 +39,10 @@ kb_test_qty.row('5', '7', '10', '15')
 
 kb_test_mistakes = telebot.types.ReplyKeyboardMarkup(True, True)
 kb_test_mistakes.row('Просмотреть ошибки', 'Выйти')
+
+kb_test_end = types.InlineKeyboardMarkup(row_width=2)
+kb_test_end_ = types.InlineKeyboardButton(text="Закончить прохождение теста", callback_data="end")
+kb_test_end.add(kb_test_end_)
 
 adm = 'f297a57a5a743894a0e4a801fc3'
 
